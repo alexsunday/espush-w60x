e59e0c32de36b04df02df7ec20b1a68a0ff35419
espush <webmaster@espush.cn>
2019-10-09 17:51:05 +0800

    基本完成 finder； 加入 mdns 服务；

--- ports/espush/src/finder.c
+++ ports/espush/src/finder.c
@@ -1,5 +1,6 @@
 #include <rtthread.h>
 #include <string.h>
+#include "utils.h"
 
 #if !defined(SAL_USING_POSIX)
 #error "Please enable SAL_USING_POSIX!"
@@ -10,8 +11,6 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <ulog.h>
-
-#define BUFSZ   1024
 
 static int is_running = 0;
 static int port = 21502;
@@ -30,35 +29,51 @@ void wait_network(void)
     }
 }
 
-static void udpserv(void *paramemter)
+/*
+=> beep_espush_finder
+<= beep_:imei
+当下比较简单，只做设备发现
+收到固定字符串后，返回 IMEI，即可
+*/
+static void handle_recv(int sock, const char* data, size_t length, struct sockaddr_in* client)
 {
-    int sock;
-    int flag = 1;
+    int rc;
+    char rsp_buf[32];
+    /* 输出接收的数据 */
+    LOG_I("Received data = %s", data);
+    const char beep[] = "beep_espush_finder";
+    const char* prefix = "beep_";
+
+    if(!rt_memcmp(data, beep, rt_strlen(beep))) {
+        // 返回 IMEI
+        LOG_I("recv finder password.");
+        rt_memset(rsp_buf, 0, sizeof(rsp_buf));
+
+        rt_memcpy(rsp_buf, prefix, rt_strlen(prefix));
+        get_imei(rsp_buf + rt_strlen(prefix));
+
+        rc = sendto(sock, rsp_buf, rt_strlen(rsp_buf), 0,
+            (struct sockaddr*)client, sizeof(struct sockaddr_in));
+        if(rc < 0) {
+            LOG_E("send udp failed. %d", rc);
+        }
+    }
+}
+
+static int finder_main(int sock)
+{
     int bytes_read;
-    char *recv_data;
-    socklen_t addr_len;
+    int flag = 1;
+    uint32_t addr_len;
+    char recv_data[32];
+
+    fd_set readset;
+    struct timeval timeout;
     struct sockaddr_in server_addr, client_addr;
 
-    struct timeval timeout;
-    fd_set readset;
-
-    wait_network();
-    /* 分配接收用的数据缓冲 */
-    recv_data = rt_malloc(BUFSZ);
-    if (recv_data == RT_NULL)
-    {
-        LOG_E("No memory");
-        return;
-    }
-
-    /* 创建一个socket，类型是SOCK_DGRAM，UDP类型 */
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-    {
-        LOG_E("Create socket error");
-        goto __exit;
-    }
+    // 配置接收广播报文
     setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &flag, sizeof(flag));
-
+    rt_memset(recv_data, 0, sizeof(recv_data));
     /* 初始化服务端地址 */
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
@@ -67,10 +82,9 @@ static void udpserv(void *paramemter)
 
     /* 绑定socket到服务端地址 */
     if (bind(sock, (struct sockaddr *)&server_addr,
-             sizeof(struct sockaddr)) == -1)
-    {
+             sizeof(struct sockaddr)) == -1) {
         LOG_E("Unable to bind");
-        goto __exit;
+        return -1;
     }
 
     addr_len = sizeof(struct sockaddr);
@@ -80,8 +94,7 @@ static void udpserv(void *paramemter)
     timeout.tv_sec = 3;
     timeout.tv_usec = 0;
 
-    while (is_running)
-    {
+    while (is_running) {
         FD_ZERO(&readset);
         FD_SET(sock, &readset);
 
@@ -90,45 +103,48 @@ static void udpserv(void *paramemter)
             continue;
 
         /* 从sock中收取最大BUFSZ - 1字节数据 */
-        bytes_read = recvfrom(sock, recv_data, BUFSZ - 1, 0,
+        bytes_read = recvfrom(sock, recv_data, sizeof(recv_data) - 1, 0,
                               (struct sockaddr *)&client_addr, &addr_len);
-        if (bytes_read < 0)
-        {
+        if (bytes_read < 0) {
             LOG_E("Received error, close the connect.");
-            goto __exit;
-        }
-        else if (bytes_read == 0)
-        {
+            return -1;
+        } else if (bytes_read == 0) {
             LOG_W("Received warning, recv function return 0.");
             continue;
-        }
-        else
-        {
+        } else {
             recv_data[bytes_read] = '\0'; /* 把末端清零 */
-
-            /* 输出接收的数据 */
-            LOG_I("Received data = %s", recv_data);
-
-            /* 如果接收数据是exit，退出 */
-            if (strcmp(recv_data, "exit") == 0)
-            {
-                goto __exit;
-            }
+            handle_recv(sock, recv_data, bytes_read, &client_addr);
         }
     }
 
-__exit:
-    if (recv_data)
-    {
-        rt_free(recv_data);
-        recv_data = RT_NULL;
-    }
-    if (sock >= 0)
-    {
+    return 0;
+}
+
+
+static void finder_task(void* params)
+{
+    int sock, rc;
+
+    while(1) {
+        // 等待网络就绪
+        wait_network();
+        
+        // new socket
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sock == -1) {
+            LOG_E("Create socket error");
+            rt_thread_mdelay(1000);
+            continue;
+        }
+
+        // udp 监听循环
+        rc = finder_main(sock);
+        if(rc < 0) {
+            LOG_E("finder failed.");
+        }
         closesocket(sock);
-        sock = -1;
+        is_running = 0;
     }
-    is_running = 0;
 }
 
 
@@ -136,9 +152,7 @@ int finder_init(void)
 {
     rt_thread_t tid;
 
-    tid = rt_thread_create("udp_serv",
-        udpserv, RT_NULL,
-        2048, RT_THREAD_PRIORITY_MAX/3, 20);
+    tid = rt_thread_create("finder", finder_task, RT_NULL, 2048, 24, 5);
     if (tid != RT_NULL) {
         rt_thread_startup(tid);
     }
