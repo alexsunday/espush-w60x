506aeb46e6e6eb98afa2838e32ddcf372dcf404c
espush <webmaster@espush.cn>
2019-10-14 17:28:32 +0800

    局域网模式完成设备信息发现接口，待测试； 删除 packages 下一些无用的文件；

--- ports/espush/src/local_mode.c
+++ ports/espush/src/local_mode.c
@@ -3,23 +3,48 @@
 #include <ulog.h>
 
 void wait_network(void);
+void get_imei(char* out);
 
+/*
+返回 IMEI，设备类型，使用协议，芯片型号，回路数 等等
+chip_id: W60X123456789012
+type: light
+chip: W600/W601
+lines: 1/4
+*/
 static void handle_device_info(struct mg_connection *nc, struct http_message *hm)
 {
-    char n1[100], n2[100];
-    double result;
+    char resp[128];
+    char imei[24];
+    mg_send_response_line(nc, 200, "Content-Type: text/html\r\nConnection: close");
 
-    /* Get form variables */
-    mg_get_http_var(&hm->body, "n1", n1, sizeof(n1));
-    mg_get_http_var(&hm->body, "n2", n2, sizeof(n2));
+    rt_memset(resp, 0, sizeof(resp));
+    rt_memset(imei, 0, sizeof(imei));
+    get_imei(imei);
+    mg_printf(nc, "{\"chip_id\":\"%.*s\",\"type\":\"light\",\"chip\":\"W60X\",\"lines\":4}", rt_strlen(imei), imei);
+}
 
-    /* Send headers */
-    mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
+static void handle_404(struct mg_connection *nc, struct http_message *hm)
+{
+    mg_send_response_line(nc, 404, "Content-Type: text/html\r\nConnection: close");
+    mg_printf(nc, "Not Found.");
+}
 
-    /* Compute the result and send it back as a JSON object */
-    result = strtod(n1, NULL) + strtod(n2, NULL);
-    mg_printf_http_chunk(nc, "{ \"result\": %lf }", result);
-    mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
+void handle_http_request(struct mg_connection *nc, void* ev_data)
+{
+    char addr[32];
+    struct http_message *hm = (struct http_message *) ev_data;
+
+    mg_sock_addr_to_str(&nc->sa, addr, sizeof(addr), MG_SOCK_STRINGIFY_IP | MG_SOCK_STRINGIFY_PORT);
+    LOG_I("%p: %.*s %.*s", nc, (int) hm->method.len, hm->method.p, (int) hm->uri.len, hm->uri.p);
+
+    // 这里通过 URL 进行分发
+    if(!rt_strcmp(hm->uri.p, "/device/info")) {
+        handle_device_info(nc, hm);
+    } else {
+        handle_404(nc, hm);
+    }
+    nc->flags |= MG_F_SEND_AND_CLOSE;
 }
 
 static void http_server_ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
@@ -35,15 +60,7 @@ static void http_server_ev_handler(struct mg_connection *nc, int ev, void *ev_da
         break;
     }
     case MG_EV_HTTP_REQUEST: {
-        struct http_message *hm = (struct http_message *) ev_data;
-        char addr[32];
-        mg_sock_addr_to_str(&nc->sa, addr, sizeof(addr), MG_SOCK_STRINGIFY_IP | MG_SOCK_STRINGIFY_PORT);
-        LOG_I("%p: %.*s %.*s", nc, (int) hm->method.len, hm->method.p, (int) hm->uri.len, hm->uri.p);
-        mg_send_response_line(nc, 200, "Content-Type: text/html\r\n"
-                "Connection: close");
-        mg_printf(nc, "\r\n<h1>Hello, %s!</h1>\r\n"
-                "You asked for %.*s\r\n", addr, (int) hm->uri.len, hm->uri.p);
-        nc->flags |= MG_F_SEND_AND_CLOSE;
+        handle_http_request(nc, ev_data);
         break;
     }
     case MG_EV_RECV : {
